d04a329870b8fcdc01f0b704ea9936bcd30a2713
John Tucker <jtucker@appirio.com>
2018-11-15 16:50:39 -0500

    Polyfills

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
+import '@babel/polyfill';
 import React from 'react';
 import { render } from 'react-dom';
+import 'whatwg-fetch';
 import App from './components/App';
 
 render(<App />, document.getElementById('root'));
