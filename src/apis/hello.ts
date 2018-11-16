f7f619accdb34ae2ffbb1abc1998f56348ea8b58
John Tucker <jtucker@appirio.com>
2018-11-16 09:28:21 -0500

    Checkin

---
+++ src/apis/hello.ts
@@ -0,0 +1,51 @@
+import Ajv from 'ajv';
+
+const ENDPOINT = 'http://localhost:3000';
+const SCHEMA = {
+  $schema: 'http://json-schema.org/draft-07/schema#',
+  description: 'Todo set',
+};
+
+/*
+const SCHEMA = {
+  $schema: 'http://json-schema.org/draft-07/schema#',
+  description: 'Todo set',
+  items: {
+    description: 'Todo',
+    properties: {
+      completed: {
+        description: 'Whether completed',
+        type: 'boolean',
+      },
+      id: {
+        description: 'The unique identifier',
+        type: 'integer',
+      },
+      title: {
+        description: 'The title',
+        type: 'string',
+      },
+      userId: {
+        description: 'The user id of the creator',
+        type: 'number',
+      },
+    },
+    required: ['completed', 'id', 'title', 'userId'],
+    title: 'Todo',
+    type: 'object',
+  },
+  title: 'Todo set',
+  type: 'array',
+};
+const ajv = new Ajv();
+const validate = ajv.compile(SCHEMA);
+export const fetchTodos = async () => {
+  const response = await fetch(ENDPOINT);
+  const json = await response.json();
+  const valid = validate(json);
+  if (!valid) {
+    throw new Error('500');
+  }
+  return json;
+};
+*/
